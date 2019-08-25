abb6ed8a044b41fa9196a3f192711d4f9621c0ee
noam sauer-utley <noamsauerutley@gmail.com>
2019-08-25 19:36:15 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -23,11 +23,11 @@ def shift_with_args(array)
 end
 
 def using_concat(array1, array2)
-  array.concat(array2)
+  array1.concat(array2)
 end
 
 def using_insert(array, element)
-  array.insert(element, 4)
+  array.insert(4, element)
 end
 
 def using_uniq(array)
